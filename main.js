43c011c8c3dd3d4d339db16108aae290b1226d9e
MaheshTechnicals <msvarma10001@gmail.com>
2022-02-13 10:48:29 +0530

    Format document

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 var mybtn = document.getElementById("btn");
-mybtn.addEventListener("click", function () {
-  var data =document.getElementById("num").value;
+mybtn.addEventListener("click", function() {
+  var data = document.getElementById("num").value;
   console.log(data);
 })
-
-
