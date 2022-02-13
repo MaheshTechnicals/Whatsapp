ca939bfa15eb1ff86a91e1d82a0c4e55c1600487
MaheshTechnicals <msvarma10001@gmail.com>
2022-02-13 17:50:22 +0530

    All countries Suported now

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+/*var select = document.getElementById("countryCode");
+
+var a = select.options[select.selectedIndex].value;*/
+
 var mybtn = document.getElementById("btn");
 mybtn.addEventListener("click", function() {
+  var select = document.getElementById("countryCode");
+
+  var a = select.options[select.selectedIndex].value;
   var data = document.getElementById("num").value;
   var link = "https://Wa.me/";
-  var output = link + 91 + data;
+  var output = link + a + data;
   mybtn.href = output;
-}) ;
+});
