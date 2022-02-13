8cc9223c48ad84832ee3dda585c38c8376e2e0a2
MaheshTechnicals <msvarma10001@gmail.com>
2022-02-13 12:23:47 +0530

    You don't need to add 91 now

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ var mybtn = document.getElementById("btn");
 mybtn.addEventListener("click", function() {
   var data = document.getElementById("num").value;
   var link = "https://Wa.me/";
-  var output = link + data;
+  var output = link + 91+ data;
   mybtn.href = output;
 });
